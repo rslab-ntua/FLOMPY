73cd4e9e02fd66384b890338a115a00857a69c01
Olyna <olympia_g@live.com>
2022-02-15 02:55:14 +0200

    latest updates

--- LPS2022/run.py
+++ LPS2022/run.py
@@ -1,13 +1,14 @@
 from engine.sts import sentimeseries
 
-import gc
-from edges import edge_utils as eu
+import os
+from edges.delineate import CropDelineation
 
 # datapath = "/home/tars/Desktop/RSLab/FLOMPY/Data/Sentinel-2/Example_Data/"
 # #datapath = "/home/tars/Desktop/RSLab/FLOMPY/Data/Sentinel-2/2020/"
 # AOI = "/home/tars/Desktop/RSLab/FLOMPY/Data/AOI/Flompy_ianos_aoi.geojson"
 
 datapath = "/mnt/a202d601-6efc-44f7-8408-f8322b69b445/RSLab/FLOMPY/Data/Sentinel-2/Example_Data"
+corinepath = "/mnt/a202d601-6efc-44f7-8408-f8322b69b445/RSLab/FLOMPY/Data/corine_2018/greece_2018_corine.shp"
 AOI = "/mnt/a202d601-6efc-44f7-8408-f8322b69b445/RSLab/FLOMPY/Data/AOI/Flompy_ianos_aoi.geojson"
 
 # Get data
@@ -31,16 +32,18 @@ eodata.remove_orbit("136")
 eodata.show_metadata()
 
 # create obj
-crop_map = eu.CropDelineation(eodata, datapath)
+parcels = CropDelineation(eodata, datapath, corinepath)
+# corine and scl masks
+parcels.town_mask(write=False)
+parcels.cloud_mask(write=False)
 
-# compute edge intensity (0-100) map
-crop_map.edge_intensity()
+# edge intensity (0-100) map
+parcels.edge_intensity(write=True)
 
-# cloud mask
-crop_map.cloud_mask(write=True)
-
-# crop intensity map
-crop_map.mask_ndviseries(write=True)
-
-
-print('bla')
+# ndvi series
+parcels.ndviseries(write=False)
+# fill values removed by cloud mask
+parcels.interpolate_series(
+    parcels.ndviseries,
+    parcels.ndviseries_meta,
+    outfname=os.path.join(parcels.epm_path, 'crop_prob_map.tif'))
