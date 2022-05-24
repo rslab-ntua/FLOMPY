d7c96f47dcb71e69cac98eba0de98b7d86077c8a
Kleanthis Karamvasis <karamvasis_k@hotmail.com>
2022-05-24 10:42:29 +0300

    Update FLOMPYapp.py

--- flompy/FLOMPYapp.py
+++ flompy/FLOMPYapp.py
@@ -259,6 +259,7 @@ class FloodwaterEstimation:
 
 
         #--- Creating directory structure
+        if not os.path.exists(self.snap_dir): os.mkdir(self.snap_dir)
         if not os.path.exists(self.projectfolder): os.mkdir(self.projectfolder)
         self.graph_dir = os.path.join(self.scriptsfolder,'Preprocessing_S1_data/Graphs')
         assert os.path.exists(self.graph_dir)
